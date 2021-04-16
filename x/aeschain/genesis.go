6e0978b17e84a8b8744b48ea06fc619605fd6bf6
Developer Experience team at Tendermint <hello@tendermint.com>
2021-04-17 02:00:48 +0700

    Initialized with Starport

---
+++ x/aeschain/genesis.go
@@ -0,0 +1,22 @@
+package aeschain
+
+import (
+	"github.com/aeternalism/aeschain/x/aeschain/keeper"
+	"github.com/aeternalism/aeschain/x/aeschain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	// this line is used by starport scaffolding # genesis/module/init
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+
+	// this line is used by starport scaffolding # genesis/module/export
+
+	return genesis
+}
